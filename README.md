d0ffa827b8bb7fbec766f91f5979222908bcb2a1
RAjCode <krishnaraj822@gmail.com>
2021-02-17 22:33:48 +0530

    Commit message

---
+++ README.md
@@ -0,0 +1,1 @@
+# Django3-personal-portfolio
---
+++ portfolio/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Project(models.Model):
+     title = models.CharField(max_length=50)
+     description = models.CharField(max_length=200)
+     image = models.ImageField(upload_to='portfolio/image/')
+     url = models.URLField(blank=True)
+
+     def __str__(self):
+          return self.title
